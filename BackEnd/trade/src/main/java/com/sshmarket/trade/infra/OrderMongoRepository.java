a023b97bea1e733cb6be9b35273b1d0fbd60f3c8
윤경민 <yoon1994kr@naver.com>
2023-09-13 15:06:31 +0900

    refactor: 아키텍처 구조 변경

--- BackEnd/trade/src/main/java/com/sshmarket/trade/infra/OrderMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sshmarket.trade.infra;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrderMongoRepository extends MongoRepository<OrderCurrent, String> {
-}
