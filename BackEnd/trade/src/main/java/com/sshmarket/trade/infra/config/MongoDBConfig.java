0c9a0c446c233893c2884a863d68a8161c7a9d21
sujeong <msj1506@naver.com>
2023-09-15 19:48:10 +0900

    refactor: config 패키지 생성 및 이동

--- BackEnd/trade/src/main/java/com/sshmarket/trade/infra/config/MongoDBConfig.java
+++ BackEnd/trade/src/main/java/com/sshmarket/trade/infra/config/MongoDBConfig.java
@@ -1,4 +1,4 @@
-package com.sshmarket.trade.infra;
+package com.sshmarket.trade.infra.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
