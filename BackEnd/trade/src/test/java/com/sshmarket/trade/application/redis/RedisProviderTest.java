bdc624b9c93784cdc841805ae495732d8c0cf539
윤경민 <yoon1994kr@naver.com>
2023-09-20 18:00:22 +0900

    feat: 테스트 코드 작성중

--- BackEnd/trade/src/test/java/com/sshmarket/trade/application/redis/RedisProviderTest.java
+++ BackEnd/trade/src/test/java/com/sshmarket/trade/application/redis/RedisProviderTest.java
@@ -1,6 +1,7 @@
 package com.sshmarket.trade.application.redis;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,8 +20,10 @@ class RedisProviderTest {
     public void Redis_값_저장_테스트(){
         redisProvider.saveConnectionUser(1L, 1L);
 
-        for (String member : redisTemplate.opsForSet().members()) {
+        for (String member : redisTemplate.opsForSet().members(String.valueOf(1L))) {
             System.out.println("member = " + member);
+            Assertions.assertThat(member).isEqualTo("1");
         }
+
     }
 }
