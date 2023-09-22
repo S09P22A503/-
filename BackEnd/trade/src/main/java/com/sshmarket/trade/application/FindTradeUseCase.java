0b3e550cb43cdbe4bc455953529b43facc42e43c
sujeong <msj1506@naver.com>
2023-09-22 10:36:07 +0900

    feat: 거래 채팅방 마지막 메시지 시간 순 정렬

--- BackEnd/trade/src/main/java/com/sshmarket/trade/application/FindTradeUseCase.java
+++ BackEnd/trade/src/main/java/com/sshmarket/trade/application/FindTradeUseCase.java
@@ -1,5 +1,7 @@
 package com.sshmarket.trade.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sshmarket.trade.application.repository.TradeMessageRepository;
 import com.sshmarket.trade.application.repository.TradeRepository;
 import com.sshmarket.trade.domain.Status;
@@ -11,7 +13,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +25,7 @@ public class FindTradeUseCase {
     private final TradeRepository tradeRepository;
     private final TradeMessageRepository tradeMessageRepository;
     private final MemberClient memberClient;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public List<TradeResponseDto> findTrades(Long memberId, Status status) {
         List<TradeResponseDto> tradeResponseList = new ArrayList<>();
@@ -27,14 +33,23 @@ public class FindTradeUseCase {
         for (Trade trade : trades) {
             //상대방 정보 조회
             Long traderId = trade.findTrader(memberId);
-            MemberResponseDto memberResponseDto = (MemberResponseDto) memberClient.memberDetail(traderId).getBody();
+            Object memberResponse = ((LinkedHashMap) memberClient.memberDetail(traderId).getBody()).get("data");
+            MemberResponseDto memberResponseDto = null;
+            try {
+                memberResponseDto = objectMapper.readValue(objectMapper.writeValueAsString(memberResponse), MemberResponseDto.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
 
             //마지막 메시지 조회
             TradeMessage tradeMessage = tradeMessageRepository.findTopByTradeIdOrderByCreatedAtDesc(trade.getId());
-
-            tradeResponseList.add(TradeResponseDto.from(trade, tradeMessage, memberResponseDto));
+            if (tradeMessage != null) {
+                tradeResponseList.add(TradeResponseDto.from(trade, tradeMessage, memberResponseDto));
+            }
         }
 
-        return tradeResponseList;
+        return tradeResponseList.stream()
+                .sorted(Comparator.comparing(TradeResponseDto::getCreatedAt).reversed())
+                .collect(Collectors.toList());
     }
 }
