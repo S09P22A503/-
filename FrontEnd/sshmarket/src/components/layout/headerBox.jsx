abfb99909d2f60e5ef76240501d8da673a102e96
namjin <skawls25@gmail.com>
2023-09-26 15:53:01 +0900

    feat: 로그인/회원가입

--- FrontEnd/sshmarket/src/components/layout/headerBox.jsx
+++ FrontEnd/sshmarket/src/components/layout/headerBox.jsx
@@ -1,12 +1,59 @@
+
 const { default: styled } = require("styled-components");
 
 
 const Container = styled.div`
-    height: 80px;
-    background-color: aquamarine;
-    border: 1px solid black;
+  height: 80px;
+  width: 100%;
+  background-color: aquamarine;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const LogoTitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BeforeLoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AfterLoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginSignup = styled.a`
+    
 `
 
 export default function Header() {
-    return <Container>{"헤더"}</Container>
+  return (
+    <Container>
+      <LogoTitleContainer>{"로고가 들어갈 자리"}</LogoTitleContainer>
+      <SearchContainer>{"검색 창"}</SearchContainer>
+      {true ? (
+        <BeforeLoginContainer>
+            <LoginSignup href="https://accounts.google.com/o/oauth2/auth?client_id=780664099270-6fkn1r7iq6p9eihagmebg9do4j1mm4vd.apps.googleusercontent.com&redirect_uri=http://localhost:3000/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email">
+                {"로그인/회원가입"}
+            </LoginSignup>
+        </BeforeLoginContainer>
+      ) : (
+        <AfterLoginContainer>{"로그인됨"}</AfterLoginContainer>
+      )}
+    </Container>
+  );
 }
--- FrontEnd/sshmarket/src/pages/Login.jsx
+++ FrontEnd/sshmarket/src/pages/Login.jsx
@@ -1,11 +1,47 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-  
+  height: 100%;
+  width: 100%;
+  text-align: center;
 `;
 
 export default function Login() {
-  return (
-    <Container></Container>
-  );
+  const [params] = useSearchParams();
+  const authCode = params.get("code");
+
+  const CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
+  const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+  useEffect(() => {
+    axios({
+      baseURL: SERVER_URL,
+      url: SERVER_URL + "login",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      params: {
+        code : authCode,
+      }
+    })
+      .then((res) => {
+        alert(res.data.message);
+        window.location.replace(CLIENT_URL);
+      })
+      .catch((e) => {
+        if (e.response.status === 303) {
+          alert("회원가입 화면으로 이동합니다.")
+          localStorage.setItem("accessToken", e.response.data.message)
+          window.location.replace(`${CLIENT_URL}signup`)
+          return;
+        }
+        alert(e.response.data.message);
+      });
+  }, []);
+
+  return <Container>{"로그인 확인 중입니다."}</Container>;
 }
