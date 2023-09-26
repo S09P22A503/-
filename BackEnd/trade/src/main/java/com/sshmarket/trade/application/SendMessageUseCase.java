92b9254172b3042acedf989f5084e0a8a1c09b0b
윤경민 <yoon1994kr@naver.com>
2023-09-26 14:45:17 +0900

    refactor: MessageDto Naming 변경

--- BackEnd/trade/src/main/java/com/sshmarket/trade/application/SendMessageUseCase.java
+++ BackEnd/trade/src/main/java/com/sshmarket/trade/application/SendMessageUseCase.java
@@ -3,7 +3,7 @@ package com.sshmarket.trade.application;
 import com.sshmarket.trade.application.kafka.KafkaProducer;
 import com.sshmarket.trade.application.repository.TradeMessageRepository;
 import com.sshmarket.trade.domain.TradeMessage;
-import com.sshmarket.trade.dto.MessageDto;
+import com.sshmarket.trade.dto.KafkaMessageDto;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +15,7 @@ public class SendMessageUseCase {
     private final KafkaProducer kafkaProducer;
     private final TradeMessageRepository tradeMessageRepository;
 
-    public TradeMessage sendMessage(MessageDto message) {
+    public TradeMessage sendMessage(KafkaMessageDto message) {
         kafkaProducer.sendWithCallback(message);
         TradeMessage tradeMessage = TradeMessage.createTradeMessage(message.getMessage(),
                 message.getTradeId(),
