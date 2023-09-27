990f3d89a66f1d2ede0829179f6d98a35e9a3b2b
namjin <skawls25@gmail.com>
2023-09-27 12:40:24 +0900

    style: 메인 이미지 추가

--- FrontEnd/sshmarket/src/pages/Main.jsx
+++ FrontEnd/sshmarket/src/pages/Main.jsx
@@ -1,18 +1,51 @@
 import styled from "styled-components";
+import ArticleCard from "../components/article/ArticleCard";
 
 const Container = styled.div`
-  background-color: black;
 `;
 
-const MainImage = styled.img`
+const MainImage = styled.video`
   width: 100%;
-  height: 50px;
+  height: auto;
+  background-color: black;
+`
+
+const CategoryBoxContainer = styled.div`
+  margin-top: 3em;
+  border: 1px solid black;
+  height: 150px;
+`
+
+const CartegoryBox = styled.div`
+  
+`
+
+const MainArticleContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  flex-direction: row;
 `
 
 export default function Main() {
   return (
     <Container>
-      <MainImage></MainImage>
+      <MainImage autoPlay muted loop>
+        <source src="mainimage.mp4"></source>
+      </MainImage>
+      <CategoryBoxContainer>
+
+      </CategoryBoxContainer>
+      <MainArticleContainer>
+        <ArticleCard></ArticleCard>
+        <ArticleCard></ArticleCard>
+        <ArticleCard></ArticleCard>
+        <ArticleCard></ArticleCard>
+        <ArticleCard></ArticleCard>
+        <ArticleCard></ArticleCard>
+        <ArticleCard></ArticleCard>
+        <ArticleCard></ArticleCard>
+      </MainArticleContainer>
     </Container>
   );
 }
