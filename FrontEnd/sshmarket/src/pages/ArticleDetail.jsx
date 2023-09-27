5b1efc517069d8dee820eaed82189772acc6696e
김혜경 <khkkhk4095@gmail.com>
2023-09-27 09:12:13 +0900

    feat: 판매글 상세 관련 컴포넌트 완성 #S09P22A503-257

--- FrontEnd/sshmarket/src/pages/ArticleDetail.jsx
+++ FrontEnd/sshmarket/src/pages/ArticleDetail.jsx
@@ -1,18 +1,17 @@
 import styled from "styled-components";
 import ArticlePk from "../components/article/ArticlePk";
-import SideBar from "../components/article/SideBar";
+import ReviewList from "../components/review/organism/ReviewList";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-const ImageContainer = styled.div``;
-
 export default function ArticleDetail() {
   return (
     <Container>
       <ArticlePk></ArticlePk>
+      <ReviewList></ReviewList>
     </Container>
   );
 }
