285b0f3ad4ad84ca678a3149f37d74d9bd28dc86
hyekyoung <khkkhk4095@gmail.com>
2023-10-01 21:59:46 +0900

    feat: review, member service feign 동기통신

---
+++ BackEnd/article/src/main/java/com/sshmarket/article/application/feignclient/ReviewFeignClient.java
@@ -0,0 +1,19 @@
+package com.sshmarket.article.application.feignclient;
+
+import com.sshmarket.article.dto.Member;
+import com.sshmarket.article.dto.ReviewResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "review", url = "{review.url}")
+public interface ReviewFeignClient {
+
+    @PostMapping(path = "/reviews/list", produces = "application/json")
+    List<ReviewResponseDto> getMemberList(@RequestBody List<Long> idList);
+
+
+
+}
