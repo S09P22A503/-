4bb6ea6c14ac1841f141e787b31857f5f08f0ec9
hyekyoung <khkkhk4095@gmail.com>
2023-10-02 15:18:32 +0900

    fix: 이미지 저장 수정

--- BackEnd/article/src/main/java/com/sshmarket/article/application/repository/ArticleImageRepository.java
+++ BackEnd/article/src/main/java/com/sshmarket/article/application/repository/ArticleImageRepository.java
@@ -3,9 +3,12 @@ package com.sshmarket.article.application.repository;
 import com.sshmarket.article.domain.Article;
 import com.sshmarket.article.domain.ArticleImage;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArticleImageRepository {
+
+    void saveImages(List<ArticleImage> images);
 
     void removeImagesById(Article article);
 
