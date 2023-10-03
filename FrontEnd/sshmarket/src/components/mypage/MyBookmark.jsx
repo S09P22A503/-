6a79e277c4624ae874e5dc950463f54a313a1654
namjin <skawls25@gmail.com>
2023-10-04 05:26:50 +0900

    feat: 리뷰 등록 axios

--- FrontEnd/sshmarket/src/components/mypage/MyBookmark.jsx
+++ FrontEnd/sshmarket/src/components/mypage/MyBookmark.jsx
@@ -1,10 +1,69 @@
 import styled from "styled-components";
+import ArticleCard from "../article/ArticleCard";
 
 const Container = styled.div`
-`
+  margin-top: 3em;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  flex-direction: row;
+`;
+
+const ArticleContainer = styled.div`
+  margin-bottom: 2em;
+`;
 
 export default function MyBookmark() {
-    return (
-        <Container>{"찜한 상품"}</Container>
-    )
+  return (
+    <Container>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+      <ArticleContainer>
+        <ArticleCard width={280}></ArticleCard>
+      </ArticleContainer>
+    </Container>
+  );
 }
+
+function setData({ member }) {
+    const CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
+    const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+  
+    let response = undefined;
+  
+    // axios({
+    //     baseURL: SERVER_URL,
+    //     url: `/reviews/my-review/?page=1&size=12`,
+    //     method: "GET",
+    //     timeout: 10000,
+    //     headers: {
+    //       "Content-Type": "application/json;charset=UTF-8",
+    //       "Access-Control-Allow-Origin": CLIENT_URL,
+    //       "Access-Control-Allow-Credentials": true,
+    //     },
+    //     withCredentials: true,
+    //   }).then((res) => {
+    //     response = res.data;
+    //   });
+  
+    return response;
+  }
