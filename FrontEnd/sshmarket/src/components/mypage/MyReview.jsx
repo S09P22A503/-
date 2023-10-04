a01ba6628a3c8ca68700b568e3da13cfd68695d6
김남진 <skawls25@gmail.com>
2023-10-04 16:50:35 +0900

    feat: 리뷰 수정 모달 프론트

--- FrontEnd/sshmarket/src/components/mypage/MyReview.jsx
+++ FrontEnd/sshmarket/src/components/mypage/MyReview.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import ReviewBox from "./../review/ReviewBox";
 import SubButton from "../Button/SubButton";
+import { useState } from "react";
+import ReviewModifyModal from "../review/ReviewModifyModal";
 
 const Container = styled.div``;
 
@@ -15,13 +17,43 @@ const ButtonContainer = styled.div`
   justify-content: space-around;
 `;
 
+const ModalContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 999;
+`;
+
 export default function MyReview() {
+  const [isOpenModifyModal, setIsOpenModifyModal] = useState(false);
+  const [targetReview, setTargetReview] = useState();
+
+  const openModifyModal = (review) => {
+    setTargetReview(review);
+    setIsOpenModifyModal(true);
+  };
+
+  const closeModal = () => {
+    setTargetReview("");
+    setIsOpenModifyModal(false);
+  };
+
   return (
     <Container>
+      <ModalContainer hidden={!isOpenModifyModal}>
+        <ReviewModifyModal
+          review={targetReview}
+          closeModal={closeModal}
+        ></ReviewModifyModal>
+      </ModalContainer>
       <ReviewContainer>
         <ReviewBox images={[]}></ReviewBox>
         <ButtonContainer>
-          <SubButton content={"수정하기"}></SubButton>
+          <SubButton
+            content={"수정하기"}
+            onClick={() => openModifyModal(undefined)}
+          ></SubButton>
           <SubButton content={"삭제하기"}></SubButton>
         </ButtonContainer>
       </ReviewContainer>
