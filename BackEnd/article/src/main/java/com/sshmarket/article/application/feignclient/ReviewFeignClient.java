92410a263dbc2fb3718c361be5b1cbc888f2fedb
김혜경 <khkkhk4095@gmail.com>
2023-10-05 09:20:37 +0900

    fix: review feign 수정

--- BackEnd/article/src/main/java/com/sshmarket/article/application/feignclient/ReviewFeignClient.java
+++ BackEnd/article/src/main/java/com/sshmarket/article/application/feignclient/ReviewFeignClient.java
@@ -3,8 +3,6 @@ package com.sshmarket.article.application.feignclient;
 import com.sshmarket.article.dto.ReviewResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -12,7 +10,7 @@ import java.util.List;
 @FeignClient(name = "review", url = "${review.url}")
 public interface ReviewFeignClient {
 
-    @GetMapping(path = "/reviews/list")
+    @GetMapping(path = "/reviews/list", produces = "application/json")
     List<ReviewResponseDto> getReviewList(@RequestParam List<Long> articleIds);
 
 
