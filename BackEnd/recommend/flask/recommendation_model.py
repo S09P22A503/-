31055e0a952c31a5c7ea9b5bc897d9959909d287
이상재 <tkdwo7699@gmail.com>
2023-10-06 05:03:29 +0900

    fix: 유저 기반 추천

--- BackEnd/recommend/flask/recommendation_model.py
+++ BackEnd/recommend/flask/recommendation_model.py
@@ -1,10 +1,11 @@
 from sparksession import CommonSparkSession
+import requests
+import json
 
 class RecommendationModel:
     __instance = None
     __model = None
     __recommendations = None
-    __nuser = 100000
 
     def __new__(cls, *args, **kwargs):
         if not cls.__instance:
@@ -14,19 +15,26 @@ class RecommendationModel:
     def set_recommendation_model(self, new_model):
         self.__model = new_model
         
+        # member 테이블의 id를 가져옴
+        try:
+            memberIds = requests.get(url="https://j9a503a.p.ssafy.io/members/all").json()["data"]
+        except:
+            memberIds = [i for i in range(1000)]
+        
         userSubset = CommonSparkSession() \
                                     .get_spark_session() \
-                                    .createDataFrame([(i,) for i in range(self.__nuser)], ["user_id"])
+                                    .createDataFrame(memberIds, ["user_id"])
         recommendationsForUser = self.__model.recommendForUserSubset(userSubset, 5)
         recommendations_list = [row.asDict() for row in recommendationsForUser.collect()]
-        self.__recommendations = recommendations_list
+        recommendations_dict = {row['user_id']: row for row in recommendations_list}
+        self.__recommendations = recommendations_dict
         
 
     def get_recommendations(self):
         return self.__recommendations
     
     def get_recommendation_for_user(self,userId):
-        return self.__recommendations[int(userId)-1]
+        return self.__recommendations.get(userId, None)
 
     def get_recommendation_model(self):
         return self.__model
