440e60c34fc3884d8fb3ab0c89b35b7e2bb0a94e
Joslynn20 <joslynnkim@gmail.com>
2023-10-06 09:31:57 +0900

    fix: 날짜 포맷팅

--- BackEnd/Review/src/main/java/com/sshmarket/review/application/dto/ReviewResponse.java
+++ BackEnd/Review/src/main/java/com/sshmarket/review/application/dto/ReviewResponse.java
@@ -2,6 +2,7 @@ package com.sshmarket.review.application.dto;
 
 import com.sshmarket.review.domain.Review;
 import com.sshmarket.review.domain.ReviewImage;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -27,18 +28,19 @@ public class ReviewResponse {
 
     private int starRating;
 
-    private LocalDateTime createdAt;
+    private String createdAt;
 
     private List<ReviewImage> images;
 
     public static ReviewResponse from(Review review, Article article, Member member) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy.MM.dd");
         return ReviewResponse.builder()
                              .id(review.getId())
                              .article(article)
                              .member(member)
                              .message(review.getMessage())
                              .starRating(review.getStarRating())
-                             .createdAt(review.getCreatedAt())
+                             .createdAt(dateFormatter.format(review.getCreatedAt()))
                              .buyHistoryId(review.getBuyHistoryId())
                              .images(review.getReviewImages())
                              .build();
